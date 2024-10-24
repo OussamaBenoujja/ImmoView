ae3dd9320d2568b288443a592a612d8cd90116f3
MohamedMoustir <itsmoustir@gmail.com>
2024-10-24 17:27:23 +0100

    style validation le input

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 
+
+
+// 
+
+
 let icone_open=document.querySelector(".icon-open");
 let icone_close=document.querySelector(".icon-close");
 let ul=document.querySelector("nav ul");
@@ -13,3 +18,39 @@ function OpenMun(){
     ul.style.display="block"
     ul.style.transform="translate(0)";
 }
+
+// Rejex
+
+
+
+function Rejex(){
+let email = document.querySelector("#email");
+let first_name = document.querySelector(".first_name");
+let last_name = document.querySelector(".last-name");
+let phone = document.querySelector(".phone") 
+   let emailn = "exommple@kkk.com";
+   
+
+
+
+
+let vlueemail=email.value
+
+let validtionEmail=/^[a-z0-9?]+@[a-z]+\.(com|net)$/ig
+
+
+
+
+
+
+
+
+if(vlueemail.test(emailn)){
+email.style.borderBottom=" 2px solid green"
+}else{
+    email.style.borderBottom="2px solid red"
+}
+
+}
+
+console.log(email);
