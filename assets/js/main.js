610361eaf39c233bd60047df13a5bf1858f9e544
MohamedMoustir <itsmoustir@gmail.com>
2024-10-25 11:23:22 +0100

    added file css and js in files assets ou file contact in page

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,12 +18,13 @@ function OpenMun() {
 let btn = document.querySelector("#btn");
 
 btn.addEventListener('click', function () {
+
     let email = document.querySelector("#email");
     let phone = document.querySelector("#phone");
     const poupupMun = document.getElementById("Poup_up");
     let inner_pop_up = document.querySelector(".inner-pop-up");
     let cards_pop_up = document.querySelector(".cards_pop_up")
-     let close_pop_up = document.querySelector(".close_pop_up")
+    let close_pop_up = document.querySelector(".close_pop_up")
     //
     let email_value = email.value;
     let phone_value = phone.value;
@@ -31,7 +32,17 @@ btn.addEventListener('click', function () {
     let validEmail = /^[a-z0-9_%]+@[a-zA-Z]+\.(com|net)$/;
     let validPhone = /^\+212\d{8}$/;
 
-    if (validEmail.test(email_value) && validPhone.test(phone_value)) {
+
+
+
+    if (email_value === '' || phone_value === '') {
+
+        cards_pop_up.style.background = "red"
+        poupupMun.style.display = "block";
+        inner_pop_up.innerHTML = "email or phone is empty "
+
+
+    } else if (validEmail.test(email_value) && validPhone.test(phone_value)) {
         poupupMun.style.display = "block";
     } else {
         cards_pop_up.style.background = "red"
@@ -39,27 +50,27 @@ btn.addEventListener('click', function () {
         inner_pop_up.innerHTML = "email or phone is not valid "
     }
     // close menu pop_up
-    close_pop_up.onclick=function(){
+    close_pop_up.onclick = function () {
         poupupMun.style.display = "none";
-        
+      
     }
 });
 
 
-let btn_one =document.querySelector(".btn-one")
-let btn_two =document.querySelector(".btn-two")
-let btn_tree =document.querySelector(".btn-tree")
+let btn_one = document.querySelector(".btn-one")
+let btn_two = document.querySelector(".btn-two")
+let btn_tree = document.querySelector(".btn-tree")
 
 
-btn_one.onclick=function(){
-    location.href="home.html"
+btn_one.onclick = function () {
+    location.href = "home.html"
 }
 
 
-btn_two.onclick=function(){
-    location.href="home.html"
+btn_two.onclick = function () {
+    location.href = "home.html"
 }
 
-btn_tree.onclick=function(){
-    location.href="home.html"
+btn_tree.onclick = function () {
+    location.href = "home.html"
 }
